62cb2401fa5d5bf88d2a5774b4c936b5ef53eb31
juan esteban torres ibarguen <jt158161@gmail.com>
2023-03-16 18:28:01 -0500

    Se cambia las clases por funciones

--- automatizador.py
+++ automatizador.py
@@ -4,217 +4,216 @@ from shutil import move
 
 
 
-class Automation:
-    def windows():
-        # verifica si la ruta docuemtos no existe 
-        if not os.path.exists("C:/Users/" + os.getlogin() + "/Documentos"):
-            # se dirige a la ruta Docuemets
-            os.chdir("C:/Users/" + os.getlogin() + "/Documents/")
-        #
-        else:
-            # se dirigie a la ruta Docuemtos
-            os.chdir("C:/Users/" + os.getlogin() + "/Documentos/")
-        
-        
-        #
-        Path("PDF").mkdir(exist_ok=True)
-        
-        #
-        Path("Word").mkdir(exist_ok=True)
-        
-        #
-        Path("Ejecutables").mkdir(exist_ok=True)
-        
-        #
-        Path("Imagenes y Videos").mkdir(exist_ok=True)
-        
-        #
-        Path("Excel").mkdir(exist_ok=True)
-        
-        #
-        Path("PowerPoint").mkdir(exist_ok=True)
-        
-        #
-        Path("Otros").mkdir(exist_ok=True)
-        
-        #
-        os.chdir("Imagenes y Videos/")
-        
-        #
-        Path("Imagenes").mkdir(exist_ok=True)
-        
-        #
-        Path("Videos").mkdir(exist_ok=True)
-        
-        #
-        os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-        #
+
+def windows():
+    # verifica si la ruta docuemtos no existe 
+    if not os.path.exists("C:/Users/" + os.getlogin() + "/Documentos"):
+        # se dirige a la ruta Docuemets
+        os.chdir("C:/Users/" + os.getlogin() + "/Documents/")
+    #
+    else:
+        # se dirigie a la ruta Docuemtos
+        os.chdir("C:/Users/" + os.getlogin() + "/Documentos/")
     
-        '''for files in os.listdir():
-            #
-            if files.endswith(".rar") or files.endswith(".zip") or files.endswith(".7z"):
-                #
-                Compresor.DescompresorHestia(files)
-        '''#        
-        rutadocumentos = ""
+    
+    #
+    Path("PDF").mkdir(exist_ok=True)
+    
+    #
+    Path("Word").mkdir(exist_ok=True)
+    
+    #
+    Path("Ejecutables").mkdir(exist_ok=True)
+    
+    #
+    Path("Imagenes y Videos").mkdir(exist_ok=True)
+    
+    #
+    Path("Excel").mkdir(exist_ok=True)
+    
+    #
+    Path("PowerPoint").mkdir(exist_ok=True)
+    
+    #
+    Path("Otros").mkdir(exist_ok=True)
+    
+    #
+    os.chdir("Imagenes y Videos/")
+    
+    #
+    Path("Imagenes").mkdir(exist_ok=True)
+    
+    #
+    Path("Videos").mkdir(exist_ok=True)
+    
+    #
+    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+    #
+
+    '''for files in os.listdir():
         #
-        if not os.path.exists("C:/Users/" + os.getlogin() + "/Documentos"):
+        if files.endswith(".rar") or files.endswith(".zip") or files.endswith(".7z"):
             #
-            rutadocumentos = "/Documents/"
-            #
+            Compresor.DescompresorHestia(files)
+    '''#        
+    rutadocumentos = ""
+    #
+    if not os.path.exists("C:/Users/" + os.getlogin() + "/Documentos"):
         #
-        else:
-            #
-            rutadocumentos = "/Documentos/"
+        rutadocumentos = "/Documents/"
         #
-        for files in os.listdir():
+    #
+    else:
+        #
+        rutadocumentos = "/Documentos/"
+    #
+    for files in os.listdir():
+        #
+        if files.endswith(".docx") or files.endswith(".docm") or files.endswith(".dotx") or files.endswith(".dotm"):
+        
             #
-            if files.endswith(".docx") or files.endswith(".docm") or files.endswith(".dotx") or files.endswith(".dotm"):
+            os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
+
+            #
+            if not os.path.exists(files):
             
                 #
-                os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
-    
+                os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+
                 #
-                if not os.path.exists(files):
-                
-                    #
-                    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-    
-                    #
-                    move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Word/")
-    
-    
-                #
-                else:
-                
-                    #
-                    print("Error el archivo ya existe")
-            #        
-            elif files.endswith(".xlsx") or files.endswith(".xlsm") or files.endswith(".xltx") or files.endswith(".xltm") or files.endswith("xlsb") or files.endswith("xlam"):
-                
-                #
-                os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
-    
-    
-                #
-                if not os.path.exists(files):
-                
-                
-                    #
-                    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-    
-    
-                    #
-                    move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
-    
-    
-                #
-                else:
-                
-                
-                    #
-                    print("El archivo ya existe")
-    
-    
+                move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Word/")
+
+
             #
-            elif files.endswith(".pptx") or files.endswith(".pptm") or files.endswith(".potx") or files.endswith(".potm") or files.endswith(".ppam") or files.endswith(".ppsx") or files.endswith(".ppsm") or files.endswith(".sldx") or files.endswith(".sldm"):
-                
-                
-                os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
-    
-                
-                if not os.path.exists(files):
-                
-                    
-                    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-    
-                    
-                    move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
-    
-                
-                else:
-                
-                    print("El archivo ya existe")
-    
-            #        
-            elif files.endswith(".svg") or files.endswith(".jpg") or files.endswith(".png") or files.endswith(".gif"):
-            
-            
-                os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Imagenes/")
-    
-                #
-                if not os.path.exists(files):
-                
-                    #
-                    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-    
-                    #
-                    move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Imagenes/")
-                #
-                else:
-                
-                    #
-                    print("El archivo ya existe")
-    
-            #        
-            elif files.endswith(".mp4") or files.endswith(".avi") or files.endswith(".mov") or files.endswith(".flv") or files.endswith(".mkv") or files.endswith(".wmv"):
-                
-                #
-                os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Videos/")
-    
-                #
-                if not os.path.exists(files):
-                
-                    #
-                    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-    
-                    #
-                    move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Videos/")
-    
-                #    
-                else:
-                
-                    #
-                    print("El archivo ya existe")
-    
-            #        
-            elif files.endswith(".pdf"):
-            
-            
-                #
-                os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "/PDF/")
-    
-                #
-                if not os.path.exists(files):
-                
-                    #
-                    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-    
-                    #
-                    move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "PDF/")
-    
-                #    
-                else:
-                
-                    #
-                    print("El archivo ya existe")
             else:
             
                 #
-                os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "/Otros/")
-    
+                print("Error el archivo ya existe")
+        #        
+        elif files.endswith(".xlsx") or files.endswith(".xlsm") or files.endswith(".xltx") or files.endswith(".xltm") or files.endswith("xlsb") or files.endswith("xlam"):
+            
+            #
+            os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
+
+
+            #
+            if not os.path.exists(files):
+            
+            
                 #
-                if not os.path.exists(files):
+                os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+
+
+                #
+                move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
+
+
+            #
+            else:
+            
+            
+                #
+                print("El archivo ya existe")
+
+
+        #
+        elif files.endswith(".pptx") or files.endswith(".pptm") or files.endswith(".potx") or files.endswith(".potm") or files.endswith(".ppam") or files.endswith(".ppsx") or files.endswith(".ppsm") or files.endswith(".sldx") or files.endswith(".sldm"):
+            
+            
+            os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
+
+            
+            if not os.path.exists(files):
+            
                 
-                    #
-                    os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
-    
-                    #
-                    move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Otros/")
+                os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+
                 
-                else:
-                
-                    #
-                    print("El archivo ya existe")
-    
-    
+                move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "PowerPoint/")
+
+            
+            else:
+            
+                print("El archivo ya existe")
+
+        #        
+        elif files.endswith(".svg") or files.endswith(".jpg") or files.endswith(".png") or files.endswith(".gif"):
+        
+        
+            os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Imagenes/")
+
+            #
+            if not os.path.exists(files):
+            
+                #
+                os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+
+                #
+                move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Imagenes/")
+            #
+            else:
+            
+                #
+                print("El archivo ya existe")
+
+        #        
+        elif files.endswith(".mp4") or files.endswith(".avi") or files.endswith(".mov") or files.endswith(".flv") or files.endswith(".mkv") or files.endswith(".wmv"):
+            
+            #
+            os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Videos/")
+
+            #
+            if not os.path.exists(files):
+            
+                #
+                os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+
+                #
+                move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Imagenes y Videos/Videos/")
+
+            #    
+            else:
+            
+                #
+                print("El archivo ya existe")
+
+        #        
+        elif files.endswith(".pdf"):
+        
+        
+            #
+            os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "/PDF/")
+
+            #
+            if not os.path.exists(files):
+            
+                #
+                os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+
+                #
+                move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "PDF/")
+
+            #    
+            else:
+            
+                #
+                print("El archivo ya existe")
+        else:
+        
+            #
+            os.chdir("C:/Users/" + os.getlogin() + rutadocumentos + "/Otros/")
+
+            #
+            if not os.path.exists(files):
+            
+                #
+                os.chdir("C:/Users/" + os.getlogin() + "/Downloads/")
+
+                #
+                move(files, "C:/Users/" + os.getlogin() + rutadocumentos + "Otros/")
+            
+            else:
+            
+                #
+                print("El archivo ya existe")
+
--- compresor.py
+++ compresor.py
@@ -1,14 +1,14 @@
 from patoolib import extract_archive, create_archive
 
 
-class Descompresor:
+def Descompresor():
     file = input("Arraste aqui el archivo a descomprimir: ")
     extract_archive(file)
 
-class Compresor:
+def Compresor():
     file = input("Arraste aqui el archivo a comprimir: ")
     create_archive(file)
 
-class DescompresorHestia:
-    def Descomprimir(file):
-        extract_archive(file)
+
+def Descomprimir(file):
+    extract_archive(file)
