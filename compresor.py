49b41a0f6f99c78b677ec3ef1e2d6e515268b058
Juan Esteban Torres Ibarguen <89664941+Black-software100@users.noreply.github.com>
2023-03-27 12:11:55 -0500

    Se eliman el metodo de comprimir

--- compresor.py
+++ compresor.py
@@ -1,13 +1,12 @@
 from patoolib import extract_archive, create_archive
+import os
+from  shutil import make_archive
 
 
 def Descompresor():
     file = input("Arraste aqui el archivo a descomprimir: ")
     extract_archive(file)
 
-def Compresor():
-    file = input("Arraste aqui el archivo a comprimir: ")
-    create_archive(file)
 
 
 def Descomprimir(file):
