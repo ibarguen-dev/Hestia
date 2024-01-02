b6ef56eecb71044375bda0f470d17e069e7b2e96
ibarguen-dev <jt158161@gmail.com>
2024-01-02 17:33:58 -0500

    Se realizan actulizaciones

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from view.Vista import Vista
 
 from controlador.controladorConfiguraciones import Configuraciones
 
-
 try:
 
     configuracionObjecto = Configuraciones()
 
-    resultadoBaseDatos  = configuracionObjecto.datosBaseDatos()
+    resultadoBaseDatos = configuracionObjecto.datosBaseDatos()
 
-    if(resultadoBaseDatos[0] == 0):
+    if resultadoBaseDatos[0] == 0:
 
         diccionarioDatos = resultadoBaseDatos[1].copy()
 
-        vista = Vista(diccionarioDatos["color"],diccionarioDatos["ubicacion"])
+        vista = Vista(diccionarioDatos["color"], diccionarioDatos["ubicacion"])
 
         vista.iniciar_aplicacion()
 
@@ -24,4 +23,3 @@ try:
 except Exception as e:
 
     print(e)
-
