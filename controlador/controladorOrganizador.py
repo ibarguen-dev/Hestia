78c1da1c24aff4b29aff53726c9f0d5ab771c7bf
Juan Esteban Torres Ibarguen <89664941+ibarguen-dev@users.noreply.github.com>
2024-01-04 12:13:33 -0500

    Se crean los metodos para la creacion y la organizacion de archivos

--- controlador/controladorOrganizador.py
+++ controlador/controladorOrganizador.py
@@ -2,156 +2,174 @@ from os import chdir, listdir, path, getlogin
 from pathlib import Path
 from shutil import move
 from time import sleep
-
+from view import vistaAlerta
 
 class controladorOrganizador():
 
     def __init__(self,ubicacion):
 
-        self.direccion = ubicacion
+        self.__direccion = ubicacion
 
-        self.carpetas(ubicacion)
+        self.__carpetas()
 
-    def carpetas(self):
+        self.__alertas = vistaAlerta.vistaAlerta()
 
-        chdir(self.direccion)
+    def __carpetas(self):
 
-        #
-        Path("PDF").mkdir(exist_ok=True)
 
-        #
-        Path("Word").mkdir(exist_ok=True)
+        try:
 
-        #
-        Path("Imagenes y Videos").mkdir(exist_ok=True)
+            chdir(self.__direccion)
 
-        #
-        Path("Excel").mkdir(exist_ok=True)
+            #
+            Path("PDF").mkdir(exist_ok=True)
 
-        #
-        Path("PowerPoint").mkdir(exist_ok=True)
+            #
+            Path("Word").mkdir(exist_ok=True)
 
-        #
-        Path("Otros").mkdir(exist_ok=True)
+            #
+            Path("Imagenes y Videos").mkdir(exist_ok=True)
 
-        #
-        chdir("Imagenes y Videos/")
+            #
+            Path("Excel").mkdir(exist_ok=True)
 
-        #
-        Path("Imagenes").mkdir(exist_ok=True)
+            #
+            Path("PowerPoint").mkdir(exist_ok=True)
 
-        Path("Videos").mkdir(exist_ok=True)
+            #
+            Path("Otros").mkdir(exist_ok=True)
 
-    def files(self,):
+            #
+            chdir("Imagenes y Videos/")
+            #
+            Path("Imagenes").mkdir(exist_ok=True)
 
-        chdir(self.direccion)
+            Path("Videos").mkdir(exist_ok=True)
 
-        for files in listdir():
 
-            ruta = self.direccion
 
-            # Documentos de word
+        except Exception as e:
 
-            if files.endswith(".docx") or files.endswith(".docm") or files.endswith(".dotx") or files.endswith(".dotm"):
+            self.__alertas.error(e)
 
-                chdir(self.direccion)
 
-                if not path.exists(ruta + "Word/" + files):
+    def archivos(self,):
+        try:
+            chdir(self.__direccion)
 
-                    move(files, ruta + "Word/")
+            for files in listdir():
 
-                else:
+                ruta = self.__direccion
 
-                    print(f"El archivo {files} ya se encuentra en la carpeta de Word")
+                # Documentos de word
 
-                    sleep(5)
+                if files.endswith(".docx") or files.endswith(".docm") or files.endswith(".dotx") or files.endswith(".dotm"):
 
-            # Documentos de excel
+                    chdir(self.__direccion)
 
-            elif files.endswith(".xlsx") or files.endswith(".xlsm") or files.endswith(".xltx") or files.endswith(
+                    if not path.exists(ruta + "/Word/" + files):
+
+                        move(files, ruta + "/Word/")
+
+                    else:
+
+                        print(f"El archivo {files} ya se encuentra en la carpeta de Word")
+
+                        sleep(5)
+
+                # Documentos de excel
+
+                elif files.endswith(".xlsx") or files.endswith(".xlsm") or files.endswith(".xltx") or files.endswith(
                     ".xltm") or files.endswith("xlsb") or files.endswith("xlam"):
 
-                chdir(self.direccion)
+                    chdir(self.__direccion)
 
-                if not path.exists(ruta + "Excel/" + files):
+                    if not path.exists(ruta + "/Excel/" + files):
 
-                    move(files, ruta + "Excel/")
+                        move(files, ruta + "/Excel/")
 
-                else:
+                    else:
 
-                    print(f"El archivo {files} ya se encuentra en la carpeta de Excel")
+                        print(f"El archivo {files} ya se encuentra en la carpeta de Excel")
 
-                    sleep(5)
-            # Documentos de powrpoint
+                        sleep(5)
+                # Documentos de powrpoint
 
-            elif files.endswith(".pptx") or files.endswith(".pptm") or files.endswith(".potx") or files.endswith(
+                elif files.endswith(".pptx") or files.endswith(".pptm") or files.endswith(".potx") or files.endswith(
                     ".potm") or files.endswith(".ppam") or files.endswith(".ppsx") or files.endswith(
                     ".ppsm") or files.endswith(".sldx") or files.endswith(".sldm"):
 
-                chdir(self.direccion)
+                    chdir(self.__direccion)
 
-                if not path.exists(ruta + "PowerPoint/" + files):
+                    if not path.exists(ruta + "/PowerPoint/" + files):
 
-                    move(files, ruta + "PowerPoint/")
+                        move(files, ruta + "/PowerPoint/")
 
-                else:
+                    else:
 
-                    print(f"El archivo {files} ya se encuentra en la carpeta de PowerPoint")
+                        print(f"El archivo {files} ya se encuentra en la carpeta de PowerPoint")
 
-                    sleep(5)
+                        sleep(5)
 
-            # Imagenes
+                # Imagenes
 
-            elif files.endswith(".svg") or files.endswith(".jpg") or files.endswith(".png") or files.endswith(
+                elif files.endswith(".svg") or files.endswith(".jpg") or files.endswith(".png") or files.endswith(
                     ".gif") or files.endswith(".jpeg"):
 
-                chdir(self.direccion)
+                    chdir(self.__direccion)
 
-                if not path.exists(ruta + "Imagenes y Videos/Imagenes/" + files):
+                    if not path.exists(ruta + "/Imagenes y Videos/Imagenes/" + files):
 
-                    print("entre")
+                        print("entre")
 
-                    move(files, ruta + "Imagenes y Videos/Imagenes/")
+                        move(files, ruta + "/Imagenes y Videos/Imagenes/")
 
-                else:
-                    print(f"El archivo {files} ya se encuentra en la carpeta de Imagen")
+                    else:
 
-                    sleep(5)
+                        print(f"El archivo {files} ya se encuentra en la carpeta de Imagen")
 
-            elif files.endswith(".mp4") or files.endswith(".avi") or files.endswith(".mov") or files.endswith(
+                        sleep(5)
+
+                elif files.endswith(".mp4") or files.endswith(".avi") or files.endswith(".mov") or files.endswith(
                     ".flv") or files.endswith(".mkv") or files.endswith(".wmv"):
 
-                chdir(self.direccion)
+                    chdir(self.__direccion)
 
-                if not path.exists(ruta + "Imagenes y Videos/Videos/" + files):
+                    if not path.exists(ruta + "/Imagenes y Videos/Videos/" + files):
 
-                    move(files, ruta + "Imagenes y Videos/Videos/")
+                        move(files, ruta + "/Imagenes y Videos/Videos/")
 
-                else:
+                    else:
 
-                    print(f"El archivo {files} ya se encuentra en la carpeta de Videos")
+                        print(f"El archivo {files} ya se encuentra en la carpeta de Videos")
 
-                    sleep(5)
+                        sleep(5)
 
             # Archivos PDF
 
-            elif files.endswith(".pdf"):
+                elif files.endswith(".pdf"):
 
-                chdir(self.direccion)
+                    chdir(self.__direccion)
 
-                if not path.exists(ruta + "PDF/" + files):
+                    if not path.exists(ruta + "/PDF/" + files):
 
-                    move(files, ruta + "PDF/")
+                        move(files, ruta + "/PDF/")
 
-                else:
+                    else:
 
-                    print(f"El archivo {files} ya se encuentra en la carpeta de PDF")
+                        print(f"El archivo {files} ya se encuentra en la carpeta de PDF")
 
-                    sleep(5)
+                        sleep(5)
 
 
-            elif files != "Word" and files != "PDF" and files != "Imagenes y Videos" and files != "Excel" and files != "PowerPoint" and files != "Otros":
+                elif files != "Word" and files != "PDF" and files != "Imagenes y Videos" and files != "Excel" and files != "PowerPoint" and files != "Otros":
 
-                chdir(self.direccion)
+                    chdir(self.__direccion)
 
-                move(files, ruta + "Otros")
+                    move(files, ruta + "/Otros")
+
+            self.__alertas.informacion("los archivos fueron organizados")
+
+        except Exception as e:
+
+            self.__alertas.error(e)
