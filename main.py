bec618e4b10de23554e97e4c8778ac6d8f60939d
Juan Esteban Torres Ibarguen <89664941+ibarguen-dev@users.noreply.github.com>
2024-01-10 14:26:07 -0500

    Se agrega la ubicación del la aplicación

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from view.Vista import Vista
-
+import os
 from controlador.controladorConfiguraciones import Configuraciones
 
 try:
-
     configuracionObjecto = Configuraciones()
 
     resultadoBaseDatos = configuracionObjecto.datosBaseDatos()
 
     if resultadoBaseDatos[0] == 0:
-
+        aplicacion = os.getcwd()
         diccionarioDatos = resultadoBaseDatos[1].copy()
 
-        vista = Vista(diccionarioDatos["color"], diccionarioDatos["ubicacion"])
+        vista = Vista(diccionarioDatos["color"], diccionarioDatos["ubicacion"],aplicacion)
 
         vista.iniciar_aplicacion()
 
