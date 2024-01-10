265859ee5f39ac939ea4f397846fdf6c8c061b30
Juan Esteban Torres Ibarguen <89664941+ibarguen-dev@users.noreply.github.com>
2024-01-10 14:28:55 -0500

    se crea el metodo para la actulización de la ruta

--- controlador/controladorConfiguraciones.py
+++ controlador/controladorConfiguraciones.py
@@ -1,6 +1,9 @@
-from os import  getlogin
-from  pathlib import Path
-from  controlador.controladoDb import  BaseDatos
+from os import getlogin, chdir, listdir
+from pathlib import Path
+from shutil import move
+from controlador.controladoDb import BaseDatos
+from Modelo.modeloConfiguracion import modeloConfiguracion
+from tkinter import messagebox
 
 class Configuraciones():
 
@@ -9,8 +12,12 @@ class Configuraciones():
 
         self.__validarBaseDatos()
 
+        self.__modeloConfiguracion = modeloConfiguracion()
+
+        self.__alertas = messagebox
 
     def __validarBaseDatos(self):
+        errores = 0
         try:
             if not Path("hestia.db").exists():
 
@@ -18,20 +25,41 @@ class Configuraciones():
 
                 respuesta = self.__baseDatos.crearBaseDatos()
                 print(respuesta)
-                if(respuesta[0] == 0):
+                if (respuesta == 0):
+                    self.__baseDatos.ingresarDatos("System", "C:/Users/" + getlogin() + "/Downloads")
 
-                    self.__baseDatos.ingresarDatos("System","C:/Users/"+ getlogin() +"/Downloads")
-
-            else:
-
-                print("si existe la base datos")
 
         except Exception as e:
-
+            errores = 2
             print(e)
 
-
+        finally:
+            if errores == 2:
+                messagebox.showerror("Error","Hubo un error al momento de crear la base de datos")
 
     def datosBaseDatos(self):
 
         return self.__baseDatos.devolverDatos()
+
+    def ingresarDatos(self, ruta, rutavieja):
+        respusta = None
+        errores = 0
+        try:
+            respusta = self.__modeloConfiguracion.validaciones(ruta)
+
+            if respusta:
+                self.__baseDatos.actulizarDatos("System",ruta)
+                chdir(rutavieja)
+                #for carpetas in listdir():
+                    #move(carpetas,ruta)
+            else:
+                errores =1
+        except Exception as e:
+            print(e)
+            errores = 2
+
+        finally:
+            if errores == 0:
+                messagebox.showinfo("Información", "Ruta guardad")
+            else:
+                messagebox.showerror("Error", "Hubo un error al momento de guardar la ruta")
